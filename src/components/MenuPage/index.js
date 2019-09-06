47738cbda5542f759ed0a1a7e6c0e5bc3623b896
Rohit Sattu <rohitsattu@outlook.com>
2019-09-06 14:38:45 -0400

    Added Search functionality

--- src/components/MenuPage/index.js
+++ src/components/MenuPage/index.js
@@ -1,11 +1,30 @@
-import React from 'react'
-import { Button, Table, Label } from 'semantic-ui-react'
+import React, {Component} from 'react'
+import { Button, Table, Label, Input } from 'semantic-ui-react'
 
-function MenuPage(props){
-    const {products, cart} = props.props
-    const onAddToCart = props.onAddToCart
+class MenuPage extends Component {
+    state = {
+        query: ''
+    }
+
+    updateQuery = (val) => {
+        this.setState((prevState, {query}) => ({
+            query: val.trim()
+        }))
+    }
+
+    render() {
+
+    const {products, cart} = this.props.props
+    const onAddToCart = this.props.onAddToCart
+    const {query} = this.state
+    
+    const filteredProducts = query === ''
+        ? products
+        : products.filter((product) => product.name.toLowerCase().includes(query.toLowerCase()))
 
     return (
+        <>
+        <Input fluid icon='search' iconPosition='left' placeholder='Search...' value={query} onChange={(event) => this.updateQuery(event.target.value)}/>
         <Table stackable selectable  style={{width: '96%',margin: '2%'}}>
             <Table.Header>
             <Table.Row>
@@ -24,7 +43,7 @@ function MenuPage(props){
             </Table.Header>
 
             <Table.Body>
-                {products.map((product) => (
+                {filteredProducts.map((product) => (
                 <Table.Row key={product._id}>
                     <Table.Cell>{product.name}</Table.Cell>
                     <Table.Cell>${parseFloat(product.cost).toFixed(2)}</Table.Cell>
@@ -34,7 +53,9 @@ function MenuPage(props){
                 ))}
             </Table.Body>
         </Table>
+        </>
     )
+                }
 }
 
 export default MenuPage
