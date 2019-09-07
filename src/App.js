5b049717c8f09ed691abe65432796a502d5939f8
Rohit Sattu <rohitsattu@outlook.com>
2019-09-06 22:54:30 -0400

    Test deploy to heroku
    Added print option

--- src/App.js
+++ src/App.js
@@ -4,14 +4,222 @@ import {Route} from 'react-router-dom'
 import CartPage from './components/CartPage'
 class App extends Component {
   state = {
-    products: [],
+    products: [
+      {
+        "_id": "5d7277dc0366d9033d5bc664",
+        "name": "Jelly Sandwich",
+        "cost": 2,
+        "menu": "Breakfast",
+        "category": "Sandwich",
+        "resto": "Newtons",
+        "__v": 0
+      },
+      {
+        "_id": "5d7277f70366d9033d5bc665",
+        "name": "Egg Sandwich",
+        "cost": 2.65,
+        "menu": "Breakfast",
+        "category": "Sandwich",
+        "resto": "Newtons",
+        "__v": 0
+      },
+      {
+        "_id": "5d7278060366d9033d5bc666",
+        "name": "Sausage Sandwich",
+        "cost": 2.75,
+        "menu": "Breakfast",
+        "category": "Sandwich",
+        "resto": "Newtons",
+        "__v": 0
+      },
+      {
+        "_id": "5d7278100366d9033d5bc667",
+        "name": "Bacon Sandwich",
+        "cost": 2.75,
+        "menu": "Breakfast",
+        "category": "Sandwich",
+        "resto": "Newtons",
+        "__v": 0
+      },
+      {
+        "_id": "5d72783f0366d9033d5bc668",
+        "name": "Bologna Sandwich",
+        "cost": 2.85,
+        "menu": "Breakfast",
+        "category": "Sandwich",
+        "resto": "Newtons",
+        "__v": 0
+      },
+      {
+        "_id": "5d72785f0366d9033d5bc669",
+        "name": "Ham Sandwich",
+        "cost": 3.5,
+        "menu": "Breakfast",
+        "category": "Sandwich",
+        "resto": "Newtons",
+        "__v": 0
+      },
+      {
+        "_id": "5d7278750366d9033d5bc66a",
+        "name": "Grilled Cheese Sandwich",
+        "cost": 2.5,
+        "menu": "Breakfast",
+        "category": "Sandwich",
+        "resto": "Newtons",
+        "__v": 0
+      },
+      {
+        "_id": "5d7278860366d9033d5bc66b",
+        "name": "Hillshire Sandwich",
+        "cost": 3.5,
+        "menu": "Breakfast",
+        "category": "Sandwich",
+        "resto": "Newtons",
+        "__v": 0
+      },
+      {
+        "_id": "5d7278ad0366d9033d5bc66c",
+        "name": "Sausage Link Sandwich",
+        "cost": 3.25,
+        "menu": "Breakfast",
+        "category": "Sandwich",
+        "resto": "Newtons",
+        "__v": 0
+      },
+      {
+        "_id": "5d7278c20366d9033d5bc66d",
+        "name": "Steak Sandwich",
+        "cost": 3.75,
+        "menu": "Breakfast",
+        "category": "Sandwich",
+        "resto": "Newtons",
+        "__v": 0
+      },
+      {
+        "_id": "5d7278e20366d9033d5bc66e",
+        "name": "Country Ham Sandwich",
+        "cost": 3.75,
+        "menu": "Breakfast",
+        "category": "Sandwich",
+        "resto": "Newtons",
+        "__v": 0
+      },
+      {
+        "_id": "5d7278ef0366d9033d5bc66f",
+        "name": "Hash Sandwich",
+        "cost": 3.75,
+        "menu": "Breakfast",
+        "category": "Sandwich",
+        "resto": "Newtons",
+        "__v": 0
+      },
+      {
+        "_id": "5d7279520366d9033d5bc670",
+        "name": "Biscuit",
+        "cost": 1.75,
+        "menu": "Breakfast",
+        "category": "Biscuit",
+        "resto": "Newtons",
+        "__v": 0
+      },
+      {
+        "_id": "5d7279690366d9033d5bc671",
+        "name": "Sausage Biscuit",
+        "cost": 2.5,
+        "menu": "Breakfast",
+        "category": "Biscuit",
+        "resto": "Newtons",
+        "__v": 0
+      },
+      {
+        "_id": "5d7279740366d9033d5bc672",
+        "name": "Bacon Biscuit",
+        "cost": 2.5,
+        "menu": "Breakfast",
+        "category": "Biscuit",
+        "resto": "Newtons",
+        "__v": 0
+      },
+      {
+        "_id": "5d72799d0366d9033d5bc673",
+        "name": "Bologna Biscuit",
+        "cost": 2.6,
+        "menu": "Breakfast",
+        "category": "Biscuit",
+        "resto": "Newtons",
+        "__v": 0
+      },
+      {
+        "_id": "5d7279ae0366d9033d5bc674",
+        "name": "Ham Biscuit",
+        "cost": 3.5,
+        "menu": "Breakfast",
+        "category": "Biscuit",
+        "resto": "Newtons",
+        "__v": 0
+      },
+      {
+        "_id": "5d7279bc0366d9033d5bc675",
+        "name": "Hillshire Biscuit",
+        "cost": 3.5,
+        "menu": "Breakfast",
+        "category": "Biscuit",
+        "resto": "Newtons",
+        "__v": 0
+      },
+      {
+        "_id": "5d7279ce0366d9033d5bc676",
+        "name": "Sausage Link Biscuit",
+        "cost": 2.7,
+        "menu": "Breakfast",
+        "category": "Biscuit",
+        "resto": "Newtons",
+        "__v": 0
+      },
+      {
+        "_id": "5d7279de0366d9033d5bc677",
+        "name": "Steak Biscuit",
+        "cost": 3.5,
+        "menu": "Breakfast",
+        "category": "Biscuit",
+        "resto": "Newtons",
+        "__v": 0
+      },
+      {
+        "_id": "5d727a030366d9033d5bc678",
+        "name": "Country Ham Biscuit",
+        "cost": 2.9,
+        "menu": "Breakfast",
+        "category": "Biscuit",
+        "resto": "Newtons",
+        "__v": 0
+      },
+      {
+        "_id": "5d727a110366d9033d5bc679",
+        "name": "Hash Biscuit",
+        "cost": 2.9,
+        "menu": "Breakfast",
+        "category": "Biscuit",
+        "resto": "Newtons",
+        "__v": 0
+      },
+      {
+        "_id": "5d727a250366d9033d5bc67a",
+        "name": "Jelly Biscuit",
+        "cost": 1.75,
+        "menu": "Breakfast",
+        "category": "Biscuit",
+        "resto": "Newtons",
+        "__v": 0
+      }
+    ],
     cart: []
   }
-  componentDidMount() {
-    fetch('http://localhost:5001/products')
-      .then((res) => res.json())
-      .then((data) => this.setState({products: data}))
-  }
+  // componentDidMount() {
+  //   fetch('http://localhost:5001/products')
+  //     .then((res) => res.json())
+  //     .then((data) => this.setState({products: data}))
+  // }
   onAddToCart = (product_id) => {
     this.setState((prevState, {cart}) => ({
         counter: prevState.cart.push(product_id)
@@ -29,7 +237,10 @@ class App extends Component {
           <MenuPage props={this.state} onAddToCart={this.onAddToCart}/>
         )}/>
         <Route path="/cart" render={() => (
-          <CartPage props={this.state} emptyCart={this.emptyCart}/>
+          <CartPage 
+          products={this.state.products}
+          cart={this.state.cart}
+          emptyCart={this.emptyCart}/>
         )}/>
         </>
     )
--- src/components/CartPage/index.js
+++ src/components/CartPage/index.js
@@ -1,62 +1,75 @@
 import React, {Component} from 'react'
 import {Redirect, Link} from 'react-router-dom'
-import {Table, Button} from 'semantic-ui-react'
+import {Table, Segment} from 'semantic-ui-react'
 
 class CartPage extends Component {
+
+    componentDidMount() {
+        if (this.props.cart.length > 0) { 
+            window.print()
+        }
+    }
+
     render() {
-        const {products, cart} = this.props.props
+        const products = this.props.products
+        const cart = this.props.cart
         const emptyCart = this.props.emptyCart
         let totalCost = 0
 
         if (cart.length < 1) {
             return <Redirect to='/' />
         } else {
-        return (
-            <Table color="green" style={{width: '96%',margin: '2%'}}>
-                <Table.Header>
-                <Table.Row>
-                    <Table.HeaderCell width={10}>Name</Table.HeaderCell>
-                    <Table.HeaderCell width='six' textAlign='right'>Cost</Table.HeaderCell>
-                </Table.Row>
-                </Table.Header>
-                <Table.Body>
-                    {cart.map((product_id) => {
-                        const matched = products.filter((product) => product._id === product_id)
-                        totalCost = totalCost + matched[0].cost
-                        return (
-                            <Table.Row key={matched[0]._id}>
-                                <Table.Cell>{matched[0].name}</Table.Cell>
-                                <Table.Cell textAlign='right'>${parseFloat(matched[0].cost).toFixed(2)}</Table.Cell>
-                            </Table.Row>
-                        )
-                    }) }
-                </Table.Body>
-                <Table.Footer>
-                <Table.Row>
-                    <Table.Cell>----------</Table.Cell>
-                    <Table.Cell textAlign='right'>----------</Table.Cell>
-                </Table.Row>
-                <Table.Row>
-                    <Table.Cell textAlign='right' positive><strong>Total</strong></Table.Cell>
-                    <Table.Cell textAlign='right' positive><strong>${parseFloat(totalCost).toFixed(2)}</strong></Table.Cell>
-                </Table.Row>
-                <Table.Row >
-                    <Table.Cell colSpan='4'>
-                    <Button
-                        floated='right'
-                        primary
-                        size='small'
-                    >
-                        Checkout
-                    </Button>
-                    <Link to="/" onClick={emptyCart}>
-                    <Button size='small' color="red" floated="right" >Cancel</Button>
-                    </Link>
-                    </Table.Cell>
-                </Table.Row>
-                </Table.Footer>
-            </Table>
-        )}
+            return (
+                <>
+                <Link to="/" onClick={emptyCart}><Segment inverted>Newton's</Segment></Link>
+                <Table color="green" style={{width: '96%',margin: '2% 2% 0 2%'}}>
+                    <Table.Header>
+                    <Table.Row>
+                        <Table.HeaderCell width={10}>Name</Table.HeaderCell>
+                        <Table.HeaderCell width='six' textAlign='right'>Cost</Table.HeaderCell>
+                    </Table.Row>
+                    </Table.Header>
+                    <Table.Body>
+                        {cart.map((product_id) => {
+                            const matched = products.filter((product) => product._id === product_id)
+                            totalCost = totalCost + matched[0].cost
+                            return (
+                                <Table.Row key={matched[0]._id}>
+                                    <Table.Cell>{matched[0].name}</Table.Cell>
+                                    <Table.Cell textAlign='right'>${parseFloat(matched[0].cost).toFixed(2)}</Table.Cell>
+                                </Table.Row>
+                            )
+                        }) }
+                    </Table.Body>
+                    <Table.Footer>
+                    <Table.Row>
+                        <Table.Cell>----------</Table.Cell>
+                        <Table.Cell textAlign='right'>----------</Table.Cell>
+                    </Table.Row>
+                    <Table.Row>
+                        <Table.Cell textAlign='right' positive><strong>Total</strong></Table.Cell>
+                        <Table.Cell textAlign='right' positive><strong>${parseFloat(totalCost).toFixed(2)}</strong></Table.Cell>
+                    </Table.Row>
+                    {/* <Table.Row className="checkouts">
+                        <Table.Cell colSpan='4'>
+                        <Button
+                            floated='right'
+                            primary
+                            size='small'
+                            onClick={() => window.print()}
+                        >
+                            Print Page
+                        </Button>
+                        <Link to="/" onClick={emptyCart}>
+                        <Button size='small' color="red" floated="right" >Cancel</Button>
+                        </Link>
+                        </Table.Cell>
+                    </Table.Row> */}
+                    </Table.Footer>
+                </Table>
+                </>
+            )
+        }
     }
 }
 
